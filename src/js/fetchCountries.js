34ccbd32213cd18d8b75c7b295f5c049ecf20def
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-08-13 13:55:18 +0300

    change error

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,9 +3,9 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
-            // if (!response.ok) {
-            //     throw new Error(Notify.failure('Oops, there is no country with that name'));
-            // }
+            if (!response.ok) {
+                throw new Error();
+            }
             return response.json()
         })
 }
