d22c09d7a8e88ac3e746d5a9d1fe96e8df3f82df
Oleksandr Demianenko <oleksandr.dem89@gmail.com>
2022-08-13 13:56:33 +0300

    remove import

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,3 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
